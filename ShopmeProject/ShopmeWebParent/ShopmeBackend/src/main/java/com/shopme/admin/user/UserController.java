4772470b224370cffbb90a268453533591ad77a8
Dustin9x <phamhuyhoang9x@gmail.com>
2023-12-26 19:31:32 +0700

    update

--- ShopmeProject/ShopmeWebParent/ShopmeBackend/src/main/java/com/shopme/admin/user/UserController.java
+++ ShopmeProject/ShopmeWebParent/ShopmeBackend/src/main/java/com/shopme/admin/user/UserController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
@@ -32,6 +33,7 @@ public class UserController {
 		user.setEnabled(true);
 		model.addAttribute("user",user);
 		model.addAttribute("listRoles",listRoles);
+		model.addAttribute("pageTitle","Create New User");
 		return "user_form";
 	}
 	
@@ -42,5 +44,20 @@ public class UserController {
 		
 		redirectAttributes.addFlashAttribute("message", "The user has been saved successfully!");
 		return "redirect:/users";
+	}
+	
+	@GetMapping("/users/edit/{id}")
+	public String editUser(@PathVariable(name = "id") Integer id, Model model, RedirectAttributes redirectAttributes) {
+		try {
+			List<Role> listRoles = service.listRoles();
+			User user = service.get(id);
+			model.addAttribute("user",user);
+			model.addAttribute("listRoles",listRoles);
+			model.addAttribute("pageTitle","Edit User (ID: " + id +")");
+			return "user_form";
+		}catch(UserNotFoundException ex) {
+			redirectAttributes.addFlashAttribute("message", "Could not found any user with Id: " + id);
+			return "redirect:/users";
+		}
 	}
 }
--- ShopmeProject/ShopmeWebParent/ShopmeBackend/src/main/java/com/shopme/admin/user/UserService.java
+++ ShopmeProject/ShopmeWebParent/ShopmeBackend/src/main/java/com/shopme/admin/user/UserService.java
@@ -1,8 +1,10 @@
 package com.shopme.admin.user;
 
 import java.util.List;
+import java.util.NoSuchElementException;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
@@ -29,7 +31,15 @@ public class UserService {
 	}
 
 	public void save(User user) {
-		encodePassword(user);
+		boolean isUpdatingUser = (user.getId() != null);
+		if(isUpdatingUser) {
+			User existingUser = userRepo.findById(user.getId()).get();
+			if(user.getPassword().isEmpty()) {
+				user.setPassword(existingUser.getPassword());
+			}
+		}else {
+			encodePassword(user);
+		}
 		userRepo.save(user);
 	}
 	
@@ -38,8 +48,23 @@ public class UserService {
 		user.setPassword(encodedPassword);
 	}
 	
-	public boolean isEmailUnique(String email) {
+	public boolean isEmailUnique(Integer id, String email) {
 		User userByEmail = userRepo.getUserByEmail(email);
-		return userByEmail == null;
+		if(userByEmail == null) return true;
+		
+		boolean isCreatingNew = (id == null);
+		
+		if(isCreatingNew && userByEmail!=null || userByEmail.getId() != id) {
+			return false;
+		}
+		return true;
+	}
+
+	public User get(Integer id) throws UserNotFoundException {
+		try {
+		return userRepo.findById(id).get();
+		} catch(NoSuchElementException ex) {
+			throw new UserNotFoundException("Could not found the user with id: " + id);
+		}
 	}
 }
