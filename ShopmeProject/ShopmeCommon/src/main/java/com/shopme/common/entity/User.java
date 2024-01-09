c907470a1b6339a965a472fd912310e2f2874bfa
Dustin9x <116355841+Dustin9x@users.noreply.github.com>
2024-01-09 17:11:23 +0700

    update

--- ShopmeProject/ShopmeCommon/src/main/java/com/shopme/common/entity/User.java
+++ ShopmeProject/ShopmeCommon/src/main/java/com/shopme/common/entity/User.java
@@ -5,6 +5,7 @@ import java.util.Set;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
@@ -38,7 +39,7 @@ public class User {
 	
 	private boolean enabled;
 
-	@ManyToMany
+	@ManyToMany (fetch = FetchType.EAGER)
 	@JoinTable(
 			name = "users_roles", 
 			joinColumns = @JoinColumn(name = "user_id"),
